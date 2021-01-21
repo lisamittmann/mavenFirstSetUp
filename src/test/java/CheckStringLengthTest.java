700915462745366cfac708866484e4855c27a090
Lisa Mittmann <li.mittmann@gmail.com>
2021-01-21 10:50:57 +0100

    Set up mvn project and insert first class and tests

--- src/test/java/CheckStringLengthTest.java
+++ src/test/java/CheckStringLengthTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,4 +30,15 @@ public class CheckStringLengthTest {
         assertEquals(false, result);
 
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Donaudampfschifffahrtsgeschellschaft, true",
+            "Pudding, false"
+    })
+    public void parameterTest(String testString, boolean expectedResult) {
+
+        assertEquals(expectedResult, CheckStringLength.stringLength(testString));
+    }
+
 }
